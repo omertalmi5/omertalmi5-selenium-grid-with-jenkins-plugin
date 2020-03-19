c9334c8e189731d5f2875594bc9bd6f52f1bf4dd
omerta <omertalmi5@gmail.com>
2020-03-19 19:17:24 +0200

    Begin make it for grid

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,16 +1,33 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static java.lang.System.setProperty;
+import static org.openqa.selenium.remote.DesiredCapabilities.chrome;
 
 public class BaseTest {
 
     protected WebDriver driver;
 
     @BeforeTest
-    public void setupDriver() {
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        this.driver = new ChromeDriver();
+    public void setupDriver() throws MalformedURLException {
+        String host = "localhost";
+        DesiredCapabilities desiredCapabilities = chrome();
+
+        if(System.getProperty("HUB_HOST") != null)
+        {
+            host = System.getProperty("HUB_HOST");
+        }
+
+        String completeUrl = "http://" + host + ":4444/wd/hub";
+
+        this.driver = new RemoteWebDriver(new URL(completeUrl), desiredCapabilities);
     }
 
     @AfterTest
