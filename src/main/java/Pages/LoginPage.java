3d2718b9815b38f66470a0d301297d0ce0cc1890
omerta <omertalmi5@gmail.com>
2020-03-19 22:39:44 +0200

    Improve test

--- src/main/java/Pages/LoginPage.java
+++ src/main/java/Pages/LoginPage.java
@@ -5,10 +5,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
+import static org.testng.Assert.assertEquals;
 
 public class LoginPage extends AbstractPage {
 
     private final String PAGE_URL = "http://newtours.demoaut.com";
+    private final String USER_NAME = "omer";
 
     @FindBy(name = "password")
     private WebElement firstNameTxt;
@@ -22,8 +24,12 @@ public class LoginPage extends AbstractPage {
         this.wait.until(visibilityOf(this.firstNameTxt));
     }
 
-    public void login()
-    {
-        this.firstNameTxt.sendKeys("Omer");
+    public void login() {
+        this.firstNameTxt.sendKeys(USER_NAME);
+        assertEquals(getCurrentValue(), USER_NAME);
+    }
+
+    private String getCurrentValue() {
+        return this.firstNameTxt.getAttribute("value");
     }
 }
